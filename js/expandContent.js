f665bff3e66119d43233d5c4897d6f3e51103399
Liam Ferris <liamkferris@hotmail.com>
2014-01-23 16:57:44 +0000

    SlideShow Semi-Fix
    OH WELL

---
+++ js/expandContent.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.contentMore').hide();
+	
+	$('.readMore').click(function() {
+		$('.contentMore').hide();
+
+		if($(this).text() == "Read More") {
+			$('.readMore').text("Read More");
+			$(this).text("Read Less");
+			$(this).parent().children('.contentMore').show();
+		}
+		else {$('.readMore').text("Read More");}
+	});
+});
---
+++ js/header.js
@@ -0,0 +1,40 @@
+//HEADER FADE WITH SCROLL
+
+$(document).ready(function() {
+  	$('header').hover(
+  		function() {
+  			header_fadeIn();
+  		},
+  		function() {
+  			if($(window).scrollTop() >0) {header_fadeOut();}
+  		}
+  	);
+  });
+  
+$(window).scroll(function() {
+	var header_isHovered = $('header').is(':hover');
+	
+	if($(window).scrollTop() > 0 && !header_isHovered) {header_fadeOut();}
+	else {header_fadeIn();}
+});
+
+function header_fadeIn() {
+	$('header').stop(true, false).fadeTo("slow", 1);
+}
+function header_fadeOut() {
+	$('header').stop(true, false).fadeTo("slow", 0.2);
+}
+
+//HEADER CHILD MENUS
+
+$(document).ready(function() {
+	$('.headerGrandchild').hide();
+	$('.headerChild').hover(
+		function() {
+			$(this).children(".headerGrandchild").show();
+		},
+		function() {
+			$(this).children(".headerGrandchild").hide();
+		}
+	);
+});
